7ae2cddfab22b7d750e86e9b0f800d94466b9a3e
PhapLuu <phapluu2k5tqt@gmail.com>
2023-12-06 22:20:47 +0700

    Update conversation controller

--- server/controllers/conversation.controller.js
+++ server/controllers/conversation.controller.js
@@ -3,7 +3,8 @@ import "mongoose";
 
 class ConversationController {
   index = async (req, res, next) => {
-    
+    const conversations = await Conversation.find();
+    res.status(200).json(conversations);
   };
 
   store = async (req, res, next) => {
@@ -11,15 +12,51 @@ class ConversationController {
   };
 
   show = async (req, res, next) => {
-    
+    try {
+      const conversation = await Conversation.findById(req.params.id);
+      if (!conversation)
+        return res.status(404).json({
+          error: "Conversation not found",
+        });
+      res.status(200).json(conversation);
+    } catch (error) {
+      next(error);
+    }
   };
 
   update = async (req, res, next) => {
-    
+    try {
+        const conversation = await Conversation.findById(req.params.id);
+        if (!conversation)
+          return res.status(404).json({
+            error: "Conversation not found",
+          });
+        const updatedConversation = await Conversation.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedConversation)
+    } catch (error) {
+        next(error)        
+    }
+
   };
 
   destroy = async (req, res, next) => {
-   
+    try {
+      const conversation = await Conversation.findById(req.params.id);
+      if (!conversation)
+        return res.status(404).json({
+          error: "Conversation not found",
+        });
+      await Conversation.findByIdAndDelete(req.params.id);
+      res.status(200).json("Deleting a conversation");
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
