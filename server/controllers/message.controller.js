2b418b89b810136d23daa068886cd69941b6a904
PhapLuu <phapluu2k5tqt@gmail.com>
2023-12-06 22:39:12 +0700

    Update message controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,26 +1,31 @@
-import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 import "mongoose";
 
-class ConversationController {
+class MessageController {
   index = async (req, res, next) => {
-    
+    try {
+      const messages = await Message.find({ conversationId: req.params.id });
+      res.status(200).json(messages)
+    } catch (error) {
+      next(error);
+    }
   };
 
   store = async (req, res, next) => {
-    
+    const message = new Message(req.body);
+    try {
+      const savedMessage = await message.save();
+      res.status(200).json(savedMessage);
+    } catch (error) {
+      next(error);
+    }
   };
 
-  show = async (req, res, next) => {
-    
-  };
+  show = async (req, res, next) => {};
 
-  update = async (req, res, next) => {
-    
-  };
+  update = async (req, res, next) => {};
 
-  destroy = async (req, res, next) => {
-   
-  };
+  destroy = async (req, res, next) => {};
 }
 
-export default new ConversationController();
+export default new MessageController();
