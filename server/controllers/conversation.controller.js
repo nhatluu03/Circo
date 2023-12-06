b66dafd24ccf96f3ba28147421a27b0b78fd6fbb
PhapLuu <phapluu2k5tqt@gmail.com>
2023-12-06 22:12:05 +0700

    Update controllers

---
+++ server/controllers/conversation.controller.js
@@ -0,0 +1,26 @@
+import Conversation from "../models/conversation.model.js";
+import "mongoose";
+
+class ConversationController {
+  index = async (req, res, next) => {
+    
+  };
+
+  store = async (req, res, next) => {
+    
+  };
+
+  show = async (req, res, next) => {
+    
+  };
+
+  update = async (req, res, next) => {
+    
+  };
+
+  destroy = async (req, res, next) => {
+   
+  };
+}
+
+export default new ConversationController();
