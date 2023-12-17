ca090b2414ec0e82134b7d9ef72543c3832f40e7
PhapLuu <phapluu2k5tqt@gmail.com>
2023-12-17 19:59:56 +0700

    Update artwork model

--- server/models/artwork.model.js
+++ server/models/artwork.model.js
@@ -1,3 +1,22 @@
+// import mongoose from "mongoose";
+// const Schema = mongoose.Schema;
+
+// const ArtworkSchema = new Schema(
+//   {
+//     talent: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//     images: [{ type: String, required: true }],
+//     likes: { type: Number, default: 0 },
+//     saves: { type: Number, default: 0 },
+//     description: { type: String },
+//     category: { type: mongoose.Schema.Types.ObjectId, ref: 'ArtCategory', required: true },
+//     price: { type: Number, required: function () { return this.forSelling; } }, // Make 'price' required based on 'forSelling'
+//     forSelling: { type: Boolean, required: true}, // New field to indicate if artwork is for selling
+//   },
+//   { timestamps: true }
+// );
+
+// const Artwork = mongoose.model("Artwork", ArtworkSchema);
+// export default Artwork;
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
@@ -9,11 +28,23 @@ const ArtworkSchema = new Schema(
     saves: { type: Number, default: 0 },
     description: { type: String },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'ArtCategory', required: true },
-    price: { type: Number, required: function () { return this.forSelling; } }, // Make 'price' required based on 'forSelling'
-    forSelling: { type: Boolean, default: false }, // New field to indicate if artwork is for selling
+    price: {
+      type: Number,
+      validate: {
+        validator: function () {
+          // If forSelling is false, ensure that price is not provided
+          return !(this.forSelling === false && this.price !== undefined);
+        },
+        message: 'Cannot set the price when forSelling is false.',
+      },
+    },
+    forSelling: { type: Boolean, required: true },
   },
   { timestamps: true }
 );
 
 const Artwork = mongoose.model("Artwork", ArtworkSchema);
 export default Artwork;
+
+
+
