48f024fc9b02666e2278174f850a61f356e3d0c2
an6122003 <an6122003>
2023-12-22 15:54:26 +0700

    a

--- server/utils/createError.js
+++
@@ -1,9 +0,0 @@
-const createError = (status, message) => {
-    const error = new Error();
-    error.status = status;
-    error.message = message;
-
-    return error;
-};
-
-export default createError;
