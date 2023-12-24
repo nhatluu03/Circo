96a96a83bba914c4b33d0e4e310ebe931cdaa018
an6122003 <an6122003>
2023-12-24 19:56:35 +0700

    add event and conversation controller

---
+++ server/controllers/conversation.controller copy.js
@@ -0,0 +1,74 @@
+import Conversation from "../models/conversation.model.js";
+import "mongoose";
+
+class ConversationController {
+  index = async (req, res, next) => {
+    const conversations = await Conversation.find();
+    res.status(200).json(conversations);
+  };
+
+  store = async (req, res, next) => {
+    const conversation = new Conversation({
+      members: [req.body.senderId, req.body.receiverId],
+    });
+    try {
+      const savedConversation = await conversation.save();
+      res.status(200).json(savedConversation);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  show = async (req, res, next) => {
+    try {
+      const conversation = await Conversation.find({
+        members: { $in: [req.params.id] },
+      });
+      if (!conversation)
+        return res.status(404).json({
+          error: "Conversation not found",
+        });
+      res.status(200).json(conversation);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  //   update = async (req, res, next) => {
+  //     try {
+  //       const conversation = await Conversation.findById(req.params.id);
+  //       if (!conversation)
+  //         return res.status(404).json({
+  //           error: "Conversation not found",
+  //         });
+  //       const updatedConversation = await Conversation.findByIdAndUpdate(
+  //         req.params.id,
+  //         {
+  //           $set: req.body,
+  //         },
+  //         { new: true }
+  //       );
+  //       res.status(200).json(updatedConversation);
+  //     } catch (error) {
+  //       next(error);
+  //     }
+  //   };
+
+  destroy = async (req, res, next) => {
+    try {
+      const conversation = await Conversation.find({
+        members: { $in: [req.params.id] },
+      });
+      if (!conversation)
+        return res.status(404).json({
+          error: "Conversation not found",
+        });
+      await Conversation.findByIdAndDelete(req.params.id);
+      res.status(200).json("Deleting a conversation");
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default new ConversationController();
