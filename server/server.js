2c1413a83aa9fb142378943d5e0c89ebf282b84b
PhapLuu <phapluu2k5tqt@gmail.com>
2024-01-01 13:22:41 +0700

    Add multer to Application

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import route from "./routes/index.js";
 import "./utils/loadEnv.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import multer from "multer";
 
 const app = express();
 app.use(cookieParser());
@@ -16,6 +17,22 @@ app.use(cors({
   origin:"http://localhost:5173",
   credentials: true,
 }));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../client/public/upload')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use(express.json());
 
